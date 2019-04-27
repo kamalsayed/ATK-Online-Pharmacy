1e2ace6b97dcf34d1f0e72128c0b532e3bc81229
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2019-04-27 21:22:24 +0200

    Add files via upload

---
+++ views/AdminBuy.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.AdminInterface')
+
+@section('section')
+    <div class="text-center" style="padding:50px 0">
+        <div class="logo">Buy Medicine</div>
+        <!-- Main Form -->
+        <div class="login-form-1">
+            <form id="register-form" class="text-left" action="{{url('/Adminbuy/payment')}}" method="post">
+                {{csrf_field()}}
+                <div class="login-form-main-message"></div>
+                <div class="main-login-form">
+                    <div class="login-group">
+                        <div class="form-group">
+                            <select class="form-control" id="medicine" name="medicine" style="width:100%;">
+                                <?php
+                                foreach($data as $dat){
+                                    echo"
+                            <option value={$dat->id}>{$dat->name} : {$dat->brand} : {$dat->price}</option>";
+                                }
+                                ?>
+                           </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="reg_Quantity" class="sr-only">Quantity</label>
+                            <input type="text" class="form-control" id="reg_Quantity" name="reg_Quantity" placeholder="Quantity">
+                        </div>
+                    </div>
+                    <button type="submit" class="login-button" name="buy"><i class="fa fa-chevron-right"></i></button>
+                </div>
+            </form>
+        </div>
+        <!-- end:Main Form -->
+    </div>
+
+
+@stop
