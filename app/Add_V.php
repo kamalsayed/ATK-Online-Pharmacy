82585ec662c98e7d106ac481a75a8de432652f03
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2019-04-27 21:24:27 +0200

    Update Add_V.php
    singelton added

--- app/Add_V.php
+++ app/Add_V.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Add_V extends Model
 {
+    public static  $instance;
+
+    public static function  getInstance(){
+        if(empty(Add_V::$instance)){
+            Add_V::$instance=new Add_V();
+        }
+        return Add_V::$instance;
+    }
     Protected $table='users';
     Protected $Key = 'id';
 }
