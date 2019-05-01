868fbd1603221f821811ed972345635ed7ef74a4
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2019-05-01 20:52:33 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+#use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\DB;
+use Barryvdh\DomPDF\Facade as PDF;
 class HomeController extends Controller
 {
+
     /**
      * Create a new controller instance.
      *
@@ -38,6 +41,26 @@ class HomeController extends Controller
             session(['type'=>'vendor']);
             return view('layout.VendorInterface');
         }
-
     }
+
+    public function generatePDF()
+    {
+
+        ob_start();
+        require view('Report'); // the one you posted in your question
+        $html = ob_get_clean();
+        $data = ['title' => 'This is the final report'];
+        $pdf = PDF::loadHTML($html, $data);
+
+     return $pdf->stream();
+    }
+    public  function showReport(){
+        $sellers=DB::select('Select s.name , m.owner_id from users s , medicines m where s.id = m.owner_id and m.owner = ? group by s.name,m.owner_id ',[0]);
+        $most= DB::table('medicines')->where('owner','=',2)->select('name')->get();
+        #$most = DB::select('SELECT name from medicines where owner = ? GROUP BY quantity DESC',[2]);
+        $best = DB::select('SELECT name from users s where s.id in (SELECT owner_id from (SELECT max(quantity),count(*) ,owner_id from medicines m WHERE owner = ? GROUP by owner_id DESC) as n)',[2]);
+        return view('Report',compact('sellers','most','best'));
+    }
+
+
 }
