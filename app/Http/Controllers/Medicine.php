a2304500a1407224d0f5bae41f65b73bc322ddbf
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2019-05-01 20:53:08 +0200

    Update Medicine.php

--- app/Http/Controllers/Medicine.php
+++ app/Http/Controllers/Medicine.php
@@ -11,7 +11,10 @@ use mysql_xdevapi\Session;
 
 class Medicine extends Controller
 {
-
+    public function vendor_list(){
+        $data = DB::select('select * from medicines where owner_id = ? ',[Auth::user()->id]);
+        return view('vendorList',compact('data'));
+    }
 
     public function index(){
         return view('AddMedicine');
@@ -19,6 +22,18 @@ class Medicine extends Controller
 
     public function store(Request $request){
             try {
+                $this->validate($request, [
+                    'file' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                    'Name' => 'required|max:10|min:3',
+                    'Price'=>'required',
+                    'Expire'=>'required',
+                    'Production'=>'required',
+                    'Quantity'=>'required',
+                    'type'=>'required',
+                    'Description'=>'required',
+                    'Brand'=>'required'
+
+                ]);
                 $medicine = Medicine_Model::getInstance();
                 $medicine->name = $request->Name;
                 $medicine->price = $request->Price;
@@ -28,7 +43,13 @@ class Medicine extends Controller
                 $medicine->quantity = $request->Quantity;
                 $medicine->type = $request->type;
                 $medicine->description = $request->Description;
-                $medicine->file = $request->file('file');
+
+                $image = $request->file('file');
+                $name = time().'.'.$image->getClientOriginalExtension();
+                $destinationPath = public_path('imgs');
+                $image->move($destinationPath, $name);
+               # $this->save();
+                $medicine->file ="imgs/".$name;
                 $medicine->owner = 0;
                 $medicine->owner_id=Auth::user()->id;
                 $medicine->save();
@@ -58,7 +79,7 @@ class Medicine extends Controller
             if($data->quantity==$requested_quantity){
 
                 if($exist){
-                    DB::update('UPDATE medicines SET quantity = ? where id = ? ', [$exist->quantity+$requested_quantity, $medicine_id]);//for medicine
+                    DB::update('UPDATE medicines SET quantity = ? where owner_id = ? ', [$exist->quantity+$requested_quantity,Auth::user()->id ]);//for medicine
                     DB::delete('delete from medicines where owner = ? and id = ?',[0, $medicine_id]);
                 }else {
                     DB::update('UPDATE medicines SET owner = ? where id = ? ', [1, $medicine_id]);//for medicine
@@ -89,7 +110,11 @@ class Medicine extends Controller
             }
             $new_safe=$admin_safe->cash-$total_price;
         DB::update('UPDATE users SET cash = ? where type = ?',[$new_safe,1]);//for admin
-
+        /*update cash
+        for vendor*/
+        $vendor=DB::selectOne('select * from users where id = ?',[$data->owner_id]);
+        $new_charge = $total_price + $vendor->cash;
+        DB::update('update users set cash = ? where id = ?',[$new_charge,$vendor->id]);
         }else{
             print("there's no enough cash in the safe");
         }
@@ -105,5 +130,61 @@ class Medicine extends Controller
         $list = DB::select('select * from medicines where owner = ?',[1]);
         return view('listMedicine',compact('list'));
     }
+    public function Show_medicine_card(){
+        $display=DB::select('SELECT * FROM medicines where owner =?',[1]);
+        return view('/Medicines',compact('display'));
+    }
+
+    public function Client_buy(Request $req){
+        $quantity = $req->reg_Quantity;
+        $medicine_id = $req->buy;
+        $med= DB::selectOne('select * from medicines where id =? ',[$medicine_id]);
+        $client = DB::selectOne('select * from users where id =? ',[Auth::user()->id]);
+        $exist=DB::selectOne('select * from medicines where owner_id = ? and name = ? and brand = ?',[$client->id,$med->name,$med->brand]);
+        if($client->cash>=($med->price * $quantity)){
+            if($med->quantity>=$quantity){
+                if($med->quantity==$quantity){
+                    if($exist){
+                        DB::update('UPDATE medicines SET quantity = ? where owner_id = ? ', [$exist->quantity+$quantity, Auth::user()->id]);//for medicine
+                        DB::delete('delete from medicines where owner = ? and id = ?',[1,$medicine_id]);
+
+                    }else{
+                        DB::update('UPDATE medicines SET owner = ? and owner_id = ? where id = ? ', [2,Auth::user()->id,$medicine_id]);//for medicine
+                    }
+                }
+                if($med->quantity>$quantity){
+                    if($exist){
+                        DB::update('UPDATE medicines SET quantity = ? where owner = ? and owner_id = ? ', [$exist->quantity+$quantity, 2,Auth::user()->id]);//for medicine
+                        DB::update('UPDATE medicines SET quantity = ? where id = ? ',[$med->quantity-$quantity,$medicine_id]);//for medicine
+                    }else {
+                        DB::update('UPDATE medicines SET quantity = ? where id = ? ', [$med->quantity - $quantity, $medicine_id]);//for medicine
+                        $medicine = Medicine_Model::getInstance();
+                        $medicine->name = $med->name;
+                        $medicine->price = $med->price;
+                        $medicine->expire_date = $med->expire_date;
+                        $medicine->production_date = $med->production_date;
+                        $medicine->brand = $med->brand;
+                        $medicine->quantity = $quantity;
+                        $medicine->type = $med->type;
+                        $medicine->description = $med->description;
+                        $medicine->file = $med->file;
+                        $medicine->owner = 2;
+                        $medicine->owner_id = Auth::user()->id;
+                        $medicine->save();
+                    }
+            }
+
+        }else{
+            print ('no enough quantity x.x ');
+            }
+            $admin = DB::selectOne('select * from users where type = ?',[1]);
+            DB::update('update users set cash = ? where type = ?',[$admin->cash +($med->price * $quantity) ,1]);
+            DB::update('update users set cash = ? where id = ?',[$client->cash-($med->price * $quantity),$client->id]);
+        }else{
+            print ('no enough cash $_$ ');
+        }
+        return $this->Show_medicine_card();
+    }
+
 
 }
